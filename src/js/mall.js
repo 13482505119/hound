b50d2441515d6646959d225be22fd0b3a9be01da
13482505119 <110bank@163.com>
2017-05-15 22:35:37 +0800

    Add dealer goods list.

--- src/js/mall.js
+++ src/js/mall.js
@@ -104,54 +104,150 @@ require(["hound", "pullLoad", "plugins/echarts/echarts.min"], function(hound, pu
             $count.html(quantity);
             $total.html('<span class="text-big">' + total.substr(0, dotIndex) + '.</span>' + total.substr(dotIndex + 1));
         });
-        
-        var myChart = echarts.init(document.getElementById('chart'));
+
         var fontSize = parseInt(document.documentElement.getAttribute("data-dpr")) * 12;
 
         // 指定图表的配置项和数据
-        var option = {
-            title: {
-                show: false,
-                text: '历史收入'
-            },
-            legend: {
-                show: false,
-                data:['销量']
-            },
-            xAxis: {
-                axisLabel: {
-                    textStyle: {
-                        fontSize: fontSize
-                    }
+        if (document.getElementById('chart-commission')) {
+            var chartCommission = echarts.init(document.getElementById('chart-commission'));
+            var optionCommission = {
+                title: {
+                    show: false,
+                    text: '历史收入'
                 },
-                data: ["一月","二月","三月","四月","五月","六月"]
-            },
-            yAxis: {
-                axisLabel: {
-                    textStyle: {
-                        fontSize: fontSize
-                    }
-                }
-            },
-            series: [{
-                name: '收入',
-                type: 'line',
-                itemStyle: {
-                    normal: {
-                        label: {
-                            show: true,
-                            textStyle: {
-                                fontFamily: '"Microsoft YaHei","Helvetica Neue",Helvetica,Arial,sans-serif',
-                                fontSize: fontSize
-                            }
+                legend: {
+                    show: false,
+                    data:['销量']
+                },
+                xAxis: {
+                    axisLabel: {
+                        textStyle: {
+                            fontSize: fontSize
+                        }
+                    },
+                    data: ["一月","二月","三月","四月","五月","六月"]
+                },
+                yAxis: {
+                    axisLabel: {
+                        textStyle: {
+                            fontSize: fontSize
                         }
                     }
                 },
-                data: [899, 1500, 3856, 4822, 3000, 5500]
-            }]
-        };
+                series: [{
+                    name: '收入',
+                    type: 'line',
+                    itemStyle: {
+                        normal: {
+                            label: {
+                                show: true,
+                                textStyle: {
+                                    fontFamily: '"Microsoft YaHei","Helvetica Neue",Helvetica,Arial,sans-serif',
+                                    fontSize: fontSize
+                                }
+                            }
+                        }
+                    },
+                    data: [899, 1500, 3856, 4822, 3000, 5500]
+                }]
+            };
+            chartCommission.setOption(optionCommission);
+        }
 
-        // 使用刚指定的配置项和数据显示图表。
-        myChart.setOption(option);
+        if (document.getElementById('chart-order')) {
+            var chartOrder = echarts.init(document.getElementById('chart-order'));
+            var optionOrder = {
+                title : {
+                    show: false
+                },
+                tooltip : {
+                    show: false,
+                    trigger: 'item',
+                    formatter: "{a} <br/>{b} : {c} ({d}%)"
+                },
+                legend: {
+                    show: false,
+                    orient : 'vertical',
+                    x : 'left',
+                    data:['已使用订单', '待使用订单', '已退款订单']
+                },
+                calculable : true,
+                series : [
+                    {
+                        name:'订单统计',
+                        type:'pie',
+                        radius : '55%',
+                        center: ['50%', '60%'],
+                        itemStyle:{
+                            normal:{
+                                label:{
+                                    show: true,
+                                    textStyle: {
+                                        fontFamily: '"Microsoft YaHei","Helvetica Neue",Helvetica,Arial,sans-serif',
+                                        fontSize: fontSize
+                                    },
+                                    formatter: '{b}\n{c} ({d}%)'
+                                },
+                                labelLine :{show:true}
+                            }
+                        },
+                        data:[
+                            {value:15, name:'已使用订单'},
+                            {value:166, name:'待使用订单'},
+                            {value:16, name:'已退款订单'}
+                        ]
+                    }
+                ]
+            };
+            chartOrder.setOption(optionOrder);
+        }
+
+        if (document.getElementById('chart-order-history')) {
+            var chartOrderHistory = echarts.init(document.getElementById('chart-order-history'));
+            var optionOrderHistory = {
+                title : {
+                    show: false
+                },
+                tooltip : {
+                    show: false,
+                    trigger: 'item',
+                    formatter: "{a} <br/>{b} : {c} ({d}%)"
+                },
+                legend: {
+                    show: false,
+                    orient : 'vertical',
+                    x : 'left',
+                    data:['已使用订单', '待使用订单', '已退款订单']
+                },
+                calculable : true,
+                series : [
+                    {
+                        name:'订单统计',
+                        type:'pie',
+                        radius : '55%',
+                        center: ['50%', '60%'],
+                        itemStyle:{
+                            normal:{
+                                label:{
+                                    show: true,
+                                    textStyle: {
+                                        fontFamily: '"Microsoft YaHei","Helvetica Neue",Helvetica,Arial,sans-serif',
+                                        fontSize: fontSize
+                                    },
+                                    formatter: '{b}\n{c} ({d}%)'
+                                },
+                                labelLine :{show:true}
+                            }
+                        },
+                        data:[
+                            {value:15, name:'已使用订单'},
+                            {value:166, name:'待使用订单'},
+                            {value:16, name:'已退款订单'}
+                        ]
+                    }
+                ]
+            };
+            chartOrderHistory.setOption(optionOrderHistory);
+        }
     });
 });
